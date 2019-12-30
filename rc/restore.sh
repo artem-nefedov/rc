f23455c1ef0dd2969199da94024a2dd2e8558321
Artem Nefedov <requiem1337@gmail.com>
2019-12-30 20:51:13 +0300

    install zsh-completions

--- rc/restore.sh
+++ rc/restore.sh
@@ -4,21 +4,34 @@ trap 'echo "$0: Error at $LINENO"; exit 1;' ERR
 hash curl
 hash git
 
-if [ "$1" = '-z' ] && [ -z "$ZSH" ]; then
-	reply_pattern='^[yY]|[yY][eE][sS]$'
+if [ "$1" = '-z' ]; then
+	if [ -z "$ZSH" ]; then
+		reply_pattern='^[yY]|[yY][eE][sS]$'
 
-	echo -n 'Install oh-my-zsh? [Y/n] '
-	read -r reply
-	if [ -z "$reply" ] || [[ "$reply" =~ $reply_pattern ]]; then
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	fi
-
-	if uname | grep -q CYGWIN && grep -q 'db_shell: /bin/bash' /etc/nsswitch.conf; then
-		echo -n 'Change default shell to zsh? [Y/n] '
+		echo -n 'Install oh-my-zsh? [Y/n] '
 		read -r reply
 		if [ -z "$reply" ] || [[ "$reply" =~ $reply_pattern ]]; then
-			sed -i 's,# db_shell: /bin/bash,db_shell: /usr/bin/zsh,' /etc/nsswitch.conf
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 		fi
+
+		if uname | grep -q CYGWIN && grep -q 'db_shell: /bin/bash' /etc/nsswitch.conf; then
+			echo -n 'Change default shell to zsh? [Y/n] '
+			read -r reply
+			if [ -z "$reply" ] || [[ "$reply" =~ $reply_pattern ]]; then
+				sed -i 's,# db_shell: /bin/bash,db_shell: /usr/bin/zsh,' /etc/nsswitch.conf
+			fi
+		fi
+	fi
+
+	zsh_completions=${ZSH_CUSTOM:-"$HOME/.oh-my-zsh/custom"}/plugins/zsh-completions
+	if [ ! -d "$zsh_completions" ]; then
+		git clone https://github.com/zsh-users/zsh-completions "$zsh_completions"
+		sed -i.bu 's/plugins=(/&zsh-completions /' ~/.zshrc
+		if ! grep -q -F 'autoload -U compinit && compinit' ~/.zshrc; then
+			echo 'autoload -U compinit && compinit' >> ~/.zshrc
+		fi
+		rm -f ~/.zcompdump
+		rm -f ~/.zshrc.bu
 	fi
 fi
 
