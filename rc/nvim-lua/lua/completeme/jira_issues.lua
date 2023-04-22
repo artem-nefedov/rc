355215ac0b37ec34bd18ec8aec9a68bda46646ff
Artem Nefedov <anefedov@aligntech.com>
2023-04-23 00:45:03 +0200

    Tab-scoped cache for jira issues

--- rc/nvim-lua/lua/completeme/jira_issues.lua
+++ rc/nvim-lua/lua/completeme/jira_issues.lua
@@ -16,54 +16,63 @@ function source:complete(_, callback)
   end
 
   local bufnr = vim.api.nvim_get_current_buf()
+  local cached = vim.t.cached_jira_issues
 
-  -- cache results
-  if not self.cache[bufnr] then
-    Job:new({
-      'curl',
-      '--silent',
-      '--get',
-      '--data-urlencode',
-      'fields=summary,description',
-      '--config',
-      vim.env['HOME'] .. '/.jira-config',
-      on_exit = function(job)
-        local result = job:result()
-        print(vim.inspect(job))
-        print(vim.inspect(table.concat(result, '')))
-        local ok, parsed = pcall(vim.json.decode, table.concat(result, ''))
-
-        if not ok then
-          enabled = false
-          return
-        end
-
-        if parsed == nil then -- make linter happy
-          enabled = false
-          return
-        end
-
-        local items = {}
-        for _, jira_issue in ipairs(parsed.issues) do
-          jira_issue.body = string.gsub(jira_issue.body or '', '\r"', '')
-
-          table.insert(items, {
-            label = string.format('[%s] ', jira_issue.key),
-            documentation = {
-              kind = 'plaintext',
-              value = string.format('[%s] %s\n\n%s', jira_issue.key, jira_issue.fields.summary,
-              jira_issue.fields.description),
-            },
-          })
-        end
-
-        callback({ items = items, isIncomplete = false })
-        self.cache[bufnr] = items
-      end,
-    }):start()
-  else
-    callback({ items = self.cache[bufnr], isIncomplete = false })
+  if cached == nil then
+    cached = self.cache[bufnr]
   end
+
+  if cached ~= nil then
+    callback({ items = cached, isIncomplete = false })
+    return
+  end
+
+  Job:new({
+    'curl',
+    '--silent',
+    '--get',
+    '--header',
+    'Content-Type: application/json',
+    '--data-urlencode',
+    'fields=summary,description',
+    '--config',
+    vim.env['HOME'] .. '/.jira-config',
+    on_exit = function(job)
+      local result = job:result()
+      print(vim.inspect(job))
+      print(vim.inspect(table.concat(result, '')))
+      local ok, parsed = pcall(vim.json.decode, table.concat(result, ''))
+
+      if not ok then
+        enabled = false
+        return
+      end
+
+      if parsed == nil then -- make linter happy
+        enabled = false
+        return
+      end
+
+      local items = {}
+      for _, jira_issue in ipairs(parsed.issues) do
+        jira_issue.body = string.gsub(jira_issue.body or '', '\r"', '')
+
+        table.insert(items, {
+          label = string.format('[%s] ', jira_issue.key),
+          documentation = {
+            kind = 'plaintext',
+            value = string.format('[%s] %s\n\n%s', jira_issue.key, jira_issue.fields.summary,
+              jira_issue.fields.description),
+          },
+        })
+      end
+
+      callback({ items = items, isIncomplete = false })
+
+      self.cache[bufnr] = items
+      vim.t.cached_jira_issues = items
+    end,
+  }):start()
 end
 
 function source.get_trigger_characters()
@@ -72,7 +81,7 @@ end
 
 function source.is_available()
   return vim.bo.filetype == 'gitcommit' or
-  (vim.bo.filetype == 'markdown' and vim.fs.basename(vim.api.nvim_buf_get_name(0)) == 'CHANGELOG.md')
+      (vim.bo.filetype == 'markdown' and vim.fs.basename(vim.api.nvim_buf_get_name(0)) == 'CHANGELOG.md')
 end
 
 require('cmp').register_source('jira_issues', source.new())
