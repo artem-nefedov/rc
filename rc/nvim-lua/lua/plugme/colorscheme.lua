258286b827c8479526336566a843fbeba7d9eedb
Artem Nefedov <anefedov@aligntech.com>
2023-04-24 00:10:04 +0200

    tune colorscheme

--- rc/nvim-lua/lua/plugme/colorscheme.lua
+++ rc/nvim-lua/lua/plugme/colorscheme.lua
@@ -4,10 +4,25 @@ return {
   config = function()
     require('kanagawa').setup({
       compile = true,
+      theme = 'wave',
       commentStyle = { italic = false },
       keywordStyle = { italic = false },
+      colors = {
+        palette = {
+          waveBlue1 = '#2E3f44' -- visual selection
+        },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+        }
+      end
     })
-    vim.cmd.colorscheme('kanagawa-wave')
+    vim.cmd.colorscheme('kanagawa')
     -- vim.cmd.KanagawaCompile() -- run manually instead
   end,
 }
