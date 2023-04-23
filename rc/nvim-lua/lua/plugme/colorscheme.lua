1032bcc6c07de4411a908e709a35e6e6bfe730cb
Artem Nefedov <anefedov@aligntech.com>
2023-04-23 22:31:17 +0200

    setup kanagawa properly

--- rc/nvim-lua/lua/plugme/colorscheme.lua
+++ rc/nvim-lua/lua/plugme/colorscheme.lua
@@ -2,6 +2,12 @@ return {
   'rebelot/kanagawa.nvim',
   priority = 1000,
   config = function()
+    require('kanagawa').setup({
+      compile = true,
+      commentStyle = { italic = false },
+      keywordStyle = { italic = false },
+    })
     vim.cmd.colorscheme('kanagawa-wave')
+    -- vim.cmd.KanagawaCompile() -- run manually instead
   end,
 }
