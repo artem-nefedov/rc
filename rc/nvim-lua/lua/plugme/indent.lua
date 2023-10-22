d9f6ba1da1984de3a6196e06e5c448185ec1bf3b
Artem Nefedov <anefedov@aligntech.com>
2023-10-22 11:48:49 +0200

    indent-blankline config update to version 3

--- rc/nvim-lua/lua/plugme/indent.lua
+++ rc/nvim-lua/lua/plugme/indent.lua
@@ -1,10 +1,10 @@
 return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
   -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help indent_blankline.txt`
+  -- See `:help indent-blankline.txt`
   opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
+    indent = { char = '┊' },
   },
 }
