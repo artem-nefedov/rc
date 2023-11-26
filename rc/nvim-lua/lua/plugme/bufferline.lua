32bc761f2d26e6113ed55e0057a55292eecbbd65
Artem Nefedov <anefedov@aligntech.com>
2023-11-26 01:56:01 +0100

    Show tabnr in bufferline

--- rc/nvim-lua/lua/plugme/bufferline.lua
+++ rc/nvim-lua/lua/plugme/bufferline.lua
@@ -1,3 +1,15 @@
+local function find_tab_by_bufnr(bufnr)
+  local winnr = vim.fn.win_findbuf(bufnr)[1]
+  for _, t in ipairs(vim.fn.gettabinfo()) do
+    for _, w in ipairs(t.windows) do
+      if w == winnr then
+        return '[' .. t.tabnr .. '] '
+      end
+    end
+  end
+  return ''
+end
+
 return {
   'akinsho/bufferline.nvim',
   version = '*',
@@ -5,9 +17,9 @@ return {
   opts = {
     options = {
       sort_by = 'tabs',
-      -- name_formatter = function(buf)
-      --   return buf.name
-      -- end,
+      name_formatter = function(buf)
+        return find_tab_by_bufnr(buf.bufnr) .. buf.name
+      end,
     },
   },
 }
