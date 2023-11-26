06167e8677706283c330a35ae6d4e29cb70210b6
Artem Nefedov <anefedov@aligntech.com>
2023-11-26 13:50:03 +0100

    check mix indent

--- rc/nvim-lua/after/plugin/statusline.lua
+++ rc/nvim-lua/after/plugin/statusline.lua
@@ -20,11 +20,32 @@ local term_extension = {
 local cache_aug = vim.api.nvim_create_augroup('lualine_cache', { clear = true })
 vim.api.nvim_create_autocmd({'CursorHold', 'BufWritePost'}, { pattern = '*', command = 'unlet! b:lualine_cache', group = cache_aug })
 
-local trailing_whitespace_detect = function()
+local whitespace_detect = function()
   if vim.b.lualine_cache == nil then
-    local space = vim.fn.search([[\s\+$]], 'nwc')
+    local indent_warn = ''
+
+    local space_indent = vim.fn.search([[^ ]], 'nwc')
+    local tab_indent = vim.fn.search([[^\t]], 'nwc')
+    local mixed = (space_indent > 0 and tab_indent > 0)
+    local mixed_same_line
+    if not mixed then
+      mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+      mixed = mixed_same_line > 0
+    end
+
+    if mixed then
+      if mixed_same_line ~= nil and mixed_same_line > 0 then
+        indent_warn = 'MI:' .. mixed_same_line
+      else
+        local space_indent_cnt = vim.fn.searchcount({pattern=[[^ ]], max_count=1e3}).total
+        local tab_indent_cnt = vim.fn.searchcount({pattern=[[^\t]], max_count=1e3}).total
+        indent_warn = 'MI:' .. (space_indent_cnt > tab_indent_cnt and tab_indent or space_indent)
+      end
+    end
+
+    local tr_space = vim.fn.search([[\s\+$]], 'nwc')
     ---@diagnostic disable-next-line: inject-field
-    vim.b.lualine_cache = (space ~= 0 and 'TW:' .. space or '')
+    vim.b.lualine_cache = indent_warn .. (tr_space ~= 0 and 'TW:' .. tr_space or '')
   end
   return vim.b.lualine_cache
 end
@@ -40,7 +61,7 @@ require('lualine').setup({
     lualine_z = {
       {'location'},
       {
-        trailing_whitespace_detect,
+        whitespace_detect,
         color={
           bg = 'orange',
         },
