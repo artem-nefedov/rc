bbe752fd87c9f0c9adcc1191739dee933df88035
Artem Nefedov <anefedov@aligntech.com>
2023-11-30 22:53:02 +0100

    treesitter-objects: conditional and loop selections

--- rc/nvim/after/plugin/treesitter.lua
+++ rc/nvim/after/plugin/treesitter.lua
@@ -48,6 +48,10 @@ require('nvim-treesitter.configs').setup({
         ['if'] = '@function.inner',
         ['ac'] = '@class.outer',
         ['ic'] = '@class.inner',
+        ['ii'] = '@conditional.inner',
+        ['ai'] = '@conditional.outer',
+        ['il'] = '@loop.inner',
+        ['al'] = '@loop.outer',
       },
     },
     move = {
