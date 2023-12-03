89d0023995ecff3fe23d31ff2d6b4ac5423d428a
Artem Nefedov <anefedov@aligntech.com>
2023-12-03 20:14:05 +0100

    oil: show hidden (except ".."), do not lcd, open persistent terminal on "ge"

--- rc/nvim/lua/plugme/oil.lua
+++ rc/nvim/lua/plugme/oil.lua
@@ -1,33 +1,35 @@
+local open_term = function()
+  vim.cmd.lcd(require("oil").get_current_dir())
+  vim.cmd.term()
+end
+
+local always_hidden = function(name, _)
+  return name == ".."
+end
+
 return {
   'stevearc/oil.nvim',
   opts = {
     keymaps = {
       ["g?"] = "actions.show_help",
-      ["<CR>"] = function()
-        require("oil.actions").select.callback()
-        vim.defer_fn(function()
-          local curdir = require("oil").get_current_dir()
-          if curdir then
-            vim.cmd.lcd(curdir)
-          end
-        end, 200)
-      end,
+      ["<CR>"] = "actions.select",
       ["<C-s>"] = "actions.select_vsplit",
       ["<C-h>"] = "actions.select_split",
       ["<C-t>"] = "actions.select_tab",
       ["<C-p>"] = "actions.preview",
       ["<C-c>"] = "actions.close",
       ["<M-l>"] = "actions.refresh",
-      ["-"] = function()
-        require("oil.actions").parent.callback()
-        vim.cmd.lcd(require("oil").get_current_dir())
-      end,
-      ["ge"] = "actions.open_terminal",
+      ["-"] = "actions.parent",
+      ["ge"] = open_term,
       ["gs"] = "actions.change_sort",
       ["gx"] = "actions.open_external",
       ["g."] = "actions.toggle_hidden",
     },
     use_default_keymaps = false,
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = always_hidden,
+    },
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
