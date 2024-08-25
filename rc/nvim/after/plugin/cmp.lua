6f899b1e36ed289af9d1fb5d310efa1f92863297
Artem Nefedov <anefedov@aligntech.com>
2024-08-25 14:44:40 +0200

    Try to do something about snippet hotkeys (from kickstart)

--- rc/nvim/after/plugin/cmp.lua
+++ rc/nvim/after/plugin/cmp.lua
@@ -44,11 +44,30 @@ cmp.setup({
         fallback()
       end
     end, { 'i', 's' }),
-    ['<M-w>']     = cmp.mapping(function(_)
-      if luasnip.expand_or_jumpable() then
+
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+
+    -- Think of <c-l> as moving to the right of your snippet expansion.
+    --  So if you have a snippet that's like:
+    --  function $name($args)
+    --    $body
+    --  end
+    --
+    -- <c-l> will move you to the right of each of the expansion locations.
+    -- <c-h> is similar, except moving you backwards.
+    ['<C-l>'] = cmp.mapping(function()
+      if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       end
     end, { 'i', 's' }),
+    ['<C-h>'] = cmp.mapping(function()
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      end
+    end, { 'i', 's' }),
+
+    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
   },
   sources = {
     { name = 'jira_issues' },
