f41bb0cf7e413852d24f98a6c16a2a195c6e4d46
Artem Nefedov <anefedov@aligntech.com>
2024-08-25 14:09:16 +0200

    gitsigns: update from kickstart

--- rc/nvim/lua/plugme/gitsigns.lua
+++ rc/nvim/lua/plugme/gitsigns.lua
@@ -1,5 +1,5 @@
 return {
-  -- Adds git releated signs to the gutter, as well as utilities for managing changes
+  -- Adds Git releated signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   opts = {
     -- See `:help gitsigns.txt`
@@ -10,29 +10,57 @@ return {
       topdelete    = { text = '‾' },
       changedelete = { text = '~' },
     },
-    on_attach = function(bufnr)
-      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Git [H]unk [P]review' })
 
-      -- don't override the built-in and fugitive keymaps
-      local gs = package.loaded.gitsigns
-      vim.keymap.set({ 'n', 'v' }, ']c', function()
+    -- Copied from kickstart
+    on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
         if vim.wo.diff then
-          return ']c'
+          vim.cmd.normal { ']c', bang = true }
+        else
+          gitsigns.nav_hunk 'next'
         end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-      vim.keymap.set({ 'n', 'v' }, '[c', function()
+      end, { desc = 'Jump to next Git [c]hange' })
+
+      map('n', '[c', function()
         if vim.wo.diff then
-          return '[c'
+          vim.cmd.normal { '[c', bang = true }
+        else
+          gitsigns.nav_hunk 'prev'
         end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+      end, { desc = 'Jump to previous Git [c]hange' })
+
+      -- Actions
+      -- visual mode
+      map('v', '<leader>hs', function()
+        gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = 'Stage Git hunk' })
+      map('v', '<leader>hr', function()
+        gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = 'Reset Git hunk' })
+      -- normal mode
+      map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Git [s]tage hunk' })
+      map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Git [r]eset hunk' })
+      map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Git [S]tage buffer' })
+      map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Git [u]ndo stage hunk' })
+      map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Git [R]eset buffer' })
+      map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Git [p]review hunk' })
+      map('n', '<leader>hb', gitsigns.blame_line, { desc = 'Git [b]lame line' })
+      map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Git [d]iff against index' })
+      map('n', '<leader>hD', function()
+        gitsigns.diffthis '@'
+      end, { desc = 'Git [D]iff against last commit' })
+      -- Toggles
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle Git show [b]lame line' })
+      map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle Git show [D]eleted' })
     end,
   },
 }
