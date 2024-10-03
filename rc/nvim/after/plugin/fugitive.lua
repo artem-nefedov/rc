7b33aaba64e6f0df78dbfba73e9191cad3f4d821
Artem Nefedov <anefedov@aligntech.com>
2024-10-03 20:31:49 +0200

    cosmetic

--- rc/nvim/after/plugin/fugitive.lua
+++ rc/nvim/after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
 -- Git menu
 vim.keymap.set('n', 'gs', vim.cmd.Git, { desc = '[G]it [S]tatus window' })
 vim.keymap.set('n', '<leader>l', '<cmd>0Gclog|copen<cr>', { desc = 'Git [L]og for current file (fugitive)' })
-vim.keymap.set('n', '<leader>rr', '<cmd>%y|Gedit|%d_|put|0d_|cclose|w<cr>', { desc = '[R]replace content of file with old revision (fugitive)' })
+vim.keymap.set('n', '<leader>rr', '<cmd>%y|Gedit|%d_|put|0d_|cclose|w<cr>', { desc = '[R]eplace content of file with old revision (fugitive)' })
