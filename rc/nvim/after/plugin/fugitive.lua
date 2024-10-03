f8b74362a72d5d93aa8bd740f59e341d1d0aebc6
Artem Nefedov <anefedov@aligntech.com>
2024-10-03 20:04:53 +0200

    Git log mapping updates

--- rc/nvim/after/plugin/fugitive.lua
+++ rc/nvim/after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
 -- Git menu
 vim.keymap.set('n', 'gs', vim.cmd.Git, { desc = '[G]it [S]tatus window' })
+vim.keymap.set('n', '<leader>l', '<cmd>0Gclog<cr>', { desc = '[G]it [L]og for current file (fugitive)' })
--- rc/nvim/after/plugin/gitlog.lua
+++ rc/nvim/after/plugin/gitlog.lua
@@ -1,1 +1,1 @@
-vim.keymap.set({'n', 'v'}, '<M-l>', require('git-log').check_log, {desc = "Check Git [L]og of selected text"})
+vim.keymap.set({'n', 'v'}, ',l', require('git-log').check_log, {desc = "Check Git [L]og of selected text"})
