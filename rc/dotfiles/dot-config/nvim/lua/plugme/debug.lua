a0978b7d21f13c5be5fbbd7b54381123e166267c
Artem Nefedov <anefedov@aligntech.com>
2025-08-02 18:30:29 +0200

    Update mason paths

--- rc/dotfiles/dot-config/nvim/lua/plugme/debug.lua
+++ rc/dotfiles/dot-config/nvim/lua/plugme/debug.lua
@@ -20,7 +20,7 @@ return {
     },
 
     -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
+    'mason-org/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
--- rc/dotfiles/dot-config/nvim/lua/plugme/lspconfig.lua
+++ rc/dotfiles/dot-config/nvim/lua/plugme/lspconfig.lua
@@ -3,9 +3,9 @@ return {
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true },
+    { 'mason-org/mason.nvim', config = true },
     -- TODO: upgrade mason-lspconfig to 2.x.x
-    { 'williamboman/mason-lspconfig.nvim', tag = 'v1.32.0' },
+    { 'mason-org/mason-lspconfig.nvim', tag = 'v1.32.0' },
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
