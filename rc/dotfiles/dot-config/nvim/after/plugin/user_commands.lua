8b74663fea46e32d1b92f0c1855126a6ef7a95b8
Artem Nefedov <anefedov@aligntech.com>
2025-12-02 00:58:49 +0100

    User command AWS

--- rc/dotfiles/dot-config/nvim/after/plugin/user_commands.lua
+++ rc/dotfiles/dot-config/nvim/after/plugin/user_commands.lua
@@ -3,4 +3,22 @@
 -- for accidental uppercase
 vim.api.nvim_create_user_command('W', ':w<bang>', { bang = true })
 
--- command! -bang -range=% -complete=file -nargs=* W <line1>,<line2>write<bang> <args>
+-- set AWS_PROFILE, and optionally AWS_REGION
+vim.api.nvim_create_user_command('AWS', function(x)
+  if table.maxn(x.fargs) == 0 then
+    print('Unset AWS_PROFILE and AWS_REGION')
+    vim.env.AWS_PROFILE = nil
+    vim.env.AWS_REGION = nil
+  elseif table.maxn(x.fargs) > 2 then
+    error('Bad number of arguments')
+    return
+  else
+    local msg = 'Set AWS_PROFILE to ' .. x.fargs[1]
+    vim.env.AWS_PROFILE = x.fargs[1]
+    if table.maxn(x.fargs) == 2 then
+      msg = msg .. ' and AWS_REGION to ' .. x.fargs[2]
+      vim.env.AWS_REGION = x.fargs[2]
+    end
+    print(msg)
+  end
+end, { nargs = '*' } )
