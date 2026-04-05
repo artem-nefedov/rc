bcf11791f908feb369649249048aa010727efcbf
Artem Nefedov <anefedov@aligntech.com>
2026-04-05 15:30:13 +0200

    Disable obsolete config for nvim-treesitter (functionality is still not fully replicated)

--- rc/dotfiles/dot-config/nvim/after/plugin/treesitter.lua
+++ rc/dotfiles/dot-config/nvim/after/plugin/treesitter.lua
@@ -1,96 +1,122 @@
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
-require('nvim-treesitter.configs').setup({
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = {
-    'go',
-    'gomod',
-    'lua',
-    'python',
-    'vimdoc',
-    'vim',
-    'bash',
-    'terraform',
-    'java',
-    'json',
-    'json5',
-    'yaml',
-    'helm',
-    'dockerfile',
-    'markdown',
-    'markdown_inline',
-    'nu',
-    'jq',
-    'xml',
-    'make',
-  },
 
-  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
+local ts = require('nvim-treesitter')
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<leader><space>',
-      node_incremental = '<leader><space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['ii'] = '@conditional.inner',
-        ['ai'] = '@conditional.outer',
-        ['il'] = '@loop.inner',
-        ['al'] = '@loop.outer',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
+ts.install({
+  'go',
+  'gomod',
+  'lua',
+  'python',
+  'vimdoc',
+  'vim',
+  'bash',
+  'terraform',
+  'java',
+  'json',
+  'json5',
+  'yaml',
+  'helm',
+  'dockerfile',
+  'markdown',
+  'markdown_inline',
+  'nu',
+  'jq',
+  'xml',
+  'make',
 })
+
+-- require('nvim-treesitter.configs').setup({
+--   -- Add languages to be installed here that you want installed for treesitter
+--   ensure_installed = {
+--     'go',
+--     'gomod',
+--     'lua',
+--     'python',
+--     'vimdoc',
+--     'vim',
+--     'bash',
+--     'terraform',
+--     'java',
+--     'json',
+--     'json5',
+--     'yaml',
+--     'helm',
+--     'dockerfile',
+--     'markdown',
+--     'markdown_inline',
+--     'nu',
+--     'jq',
+--     'xml',
+--     'make',
+--   },
+--
+--   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+--   auto_install = false,
+--
+--   highlight = {
+--     enable = true,
+--     additional_vim_regex_highlighting = false,
+--   },
+--   indent = { enable = true },
+--   incremental_selection = {
+--     enable = true,
+--     keymaps = {
+--       init_selection = '<leader><space>',
+--       node_incremental = '<leader><space>',
+--       scope_incremental = '<c-s>',
+--       node_decremental = '<M-space>',
+--     },
+--   },
+--   textobjects = {
+--     select = {
+--       enable = true,
+--       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+--       keymaps = {
+--         -- You can use the capture groups defined in textobjects.scm
+--         ['aa'] = '@parameter.outer',
+--         ['ia'] = '@parameter.inner',
+--         ['af'] = '@function.outer',
+--         ['if'] = '@function.inner',
+--         ['ac'] = '@class.outer',
+--         ['ic'] = '@class.inner',
+--         ['ii'] = '@conditional.inner',
+--         ['ai'] = '@conditional.outer',
+--         ['il'] = '@loop.inner',
+--         ['al'] = '@loop.outer',
+--       },
+--     },
+--     move = {
+--       enable = true,
+--       set_jumps = true, -- whether to set jumps in the jumplist
+--       goto_next_start = {
+--         [']m'] = '@function.outer',
+--         [']]'] = '@class.outer',
+--       },
+--       goto_next_end = {
+--         [']M'] = '@function.outer',
+--         [']['] = '@class.outer',
+--       },
+--       goto_previous_start = {
+--         ['[m'] = '@function.outer',
+--         ['[['] = '@class.outer',
+--       },
+--       goto_previous_end = {
+--         ['[M'] = '@function.outer',
+--         ['[]'] = '@class.outer',
+--       },
+--     },
+--     swap = {
+--       enable = true,
+--       swap_next = {
+--         ['<leader>a'] = '@parameter.inner',
+--       },
+--       swap_previous = {
+--         ['<leader>A'] = '@parameter.inner',
+--       },
+--     },
+--   },
+-- })
 
 vim.treesitter.language.register("helm", "gotmpl")
 -- vim.treesitter.language.register("bash", "zsh")
